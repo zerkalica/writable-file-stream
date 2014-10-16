29270463caa11e75037cfef9cdff96577964ac2c
Stefan Zerkalica <zerkalica@gmail.com>
2014-10-16 15:20:20 +0400

    bugfix

--- examples/test.js
+++ examples/test.js
@@ -1,11 +1,12 @@
-var WritableStream = require('../lib/writable-stream');
+var WritableStream = require('../lib/writable-file-stream');
 
 var f = WritableStream('./test.txt', {
 	bufferSize: 1000
 });
 
-f.write('test');
-f.write('test');
-f.write('test');
-f.write('test');
+f.write('test1');
+f.write('test2');
+f.reopen();
+f.write('test3');
+f.write('test4');
 f.end();
